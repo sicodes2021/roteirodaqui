c99c13dffe74dbc213479515d87f27962c49152f
Santunis Oliveira <santunis.oliveira@gmail.com>
2024-12-15 22:15:43 -0300

    ajustando o projeto com as funcionalidades

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,114 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar, TextInput, Platform, Pressable, ScrollView } from "react-native";
+import { MaterialIcons } from '@expo/vector-icons'
+import Slider from "@react-native-community/slider";
+
+const statusBarHeight = StatusBar.currentHeight
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" translucent={true} backgroundColor="#F1F1F1" />
+      <Text style={styles.heading}>Roteiros Daqui</Text>
+
+      <View style={styles.form}>
+        <Text style={styles.label}>Cidade destino</Text>
+        <TextInput 
+          placeholder="Ex: Brasília, DF"
+          style={styles.input}
+        />
+
+        <Text style={styles.label}>Tempo de estadia: <Text style={styles.days}> 10 </Text> dias</Text>
+
+        <Slider
+          minimumValue={1}
+          maximumValue={7}
+          minimumTrackTintColor="#009688"
+          maximumTrackTintColor="#000000"
+        />
+      </View>
+
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Gerar Roteiro</Text>
+        <MaterialIcons name="travel-explore" size={24} color="#FFF" />
+      </Pressable>
+
+      <ScrollView contentContainerStyle={{ paddingBottom: 24, marginTop: 4, }} style={styles.containerScroll} showsVerticalScrollIndicator={false}>
+        <View style={styles.content}>
+          <Text style={styles.title}>Roteiro da viagem 😍</Text>
+          <Text>Aqui vai ser o roteiro completo...</Text>
+        </View>
+      </ScrollView>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f1f1f1',
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+  heading: {
+    fontSize: 32,
+    fontWeight:'bold',
+    paddingTop: Platform.OS === 'android' ? statusBarHeight : 54
+  },
+  form: {
+    backgroundColor: '#FFF',
+    width: '90%',
+    borderRadius: 8,
+    padding: 16,
+    marginTop: 16,
+    marginBottom: 8,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#94a3b8',
+    padding: 8,
+    fontSize: 16,
+    marginBottom:  16,
+  },
+  days: {
+    backgroundColor: '#F1f1f1'
+  },
+  button: {
+    backgroundColor: '#FF5656',
+    width: '90%',
+    borderRadius: 8,
+    flexDirection: 'row',
+    padding: 14,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 8,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+  content: {
+    backgroundColor: '#FFF',
+    padding: 16,
+    width: '100%',
+    marginTop: 16,
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 14,
+  },
+  containerScroll: {
+    width: '90%',
+    marginTop: 8,
+  }
+})
